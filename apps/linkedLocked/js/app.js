8f8b896518e593b35b6051819f7dd4efbd95ff2f
Francisco Javier Abellán Abenza <franfdk17@gmail.com>
2016-04-24 20:49:38 +0200

    update

--- apps/linkedLocked/js/app.js
+++ apps/linkedLocked/js/app.js
@@ -1,4 +1,4 @@
-if (navigator.geolocation)
+/*if (navigator.geolocation)
 {
     navigator.geolocation.getCurrentPosition(showCurrentLocation);
 }
@@ -31,7 +31,24 @@ function showCurrentLocation(position)
 		map: map,
 		title: "Current location!"
 	});
+}*/
+
+
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
 }
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;  
+}
+
 
 /*$(function(){
 
