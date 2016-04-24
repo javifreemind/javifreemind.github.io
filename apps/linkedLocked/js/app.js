2dd0e5a0410a39e3e362470b7f59fc48c1297bce
Francisco Javier Abellán Abenza <franfdk17@gmail.com>
2016-04-24 22:15:04 +0200

    h

--- apps/linkedLocked/js/app.js
+++ apps/linkedLocked/js/app.js
@@ -2,6 +2,19 @@
 var x = document.getElementById("demo");
 
 
+function initialize()
+{
+    var mapProp = {
+        center:new google.maps.LatLng(51.508742,-0.120850),
+        zoom:5,
+		mapTypeId:google.maps.MapTypeId.ROADMAP
+	};
+	var map=new google.maps.Map(document.getElementById("map"),mapProp);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
 function getLocation()
 {
     if (navigator.geolocation)
